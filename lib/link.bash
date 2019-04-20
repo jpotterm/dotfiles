5ff8e7d48dd61cd18afff028a5a48ad5a3c506bf
Jonathan Potter <jpotterm@gmail.com>
2019-04-20 12:49:50 -0400

    VSCode go settings

--- lib/link.bash
+++ lib/link.bash
@@ -7,12 +7,6 @@ ln -sfh "$PWD"/config/vim/vim ~/.vim
 ln -sfh "$PWD"/config/vim/vimrc ~/.vimrc
 ln -sfh "$PWD"/config/vim/vimrc.d ~/.vimrc.d
 
-mkdir -p ~/.config/nvim
-ln -sfh ~/.vimrc ~/.config/nvim/init.vim
-ln -sfh /usr/local/bin/nvim /usr/local/bin/vi
-
-ln -sfh "$PWD"/config/vim/gvimrc ~/.gvimrc
-
 ln -sfh "$PWD"/config/isort/isort.cfg ~/.isort.cfg
 
 ln -sfh "$PWD"/config/mercurial/hgrc ~/.hgrc
